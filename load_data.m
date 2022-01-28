87c755626cb163e9cde03ce362450ce1def32138
2347965 <2347965@192.168.1.246>
2022-01-28 07:15:30 -0800

    Start putting data loading in separate function

---
+++ load_data.m
@@ -0,0 +1,45 @@
+function [tvect,fvect] = load_data(filenames, data_type, nmodes)
+% Loads data into time and frequency vectors
+% Examples provided here for loading experimental and synthetic data based
+% on data_selection
+% Input:
+%   filenames: string array with files containing data
+%   data_type: selection from noise, data, synthetic
+%   nmodes: number of modes
+%   header_lines: number of header lines in data files, if applicable
+% Output:
+%   tvect: time series vector in seconds
+%   fvect: frequency vector, each row is a different mode.
+
+% different formats of data files were used based on # of modes
+if nmodes == 2
+    colformat = '%f %f %f %f %f';
+    n_header_lines = 82;
+elseif nmodes == 3
+    colformat = '%f %f %f %f %f %f';
+    n_header_lines = 1;
+end
+
+fvect = [];
+if data_type == 'noise'
+    
+    for fi=1:nmodes   
+        fid = fopen(filenames(fi));
+        nems_data = textscan(fid, colformat,'HeaderLines', n_header_lines );
+        fclose(fid);
+        if fi == 1
+            tvect = nems_data{1};
+            tvect=tvect-tvect(1);
+        end
+        
+        freq_i = nems_data{4};
+        % optionally scale noise for simulation purpose
+        noise_factor = 1;
+        freq_i_start = freq_i(1);
+        freq_i = (freq_i-freq_i_start)*noise_factor + freq_i_start;
+        fvect = [fvect; freq_i'];
+        
+    end
+
+end
+
