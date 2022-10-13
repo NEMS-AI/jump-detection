6e82e58740f2c868fd40ddfc12756e94ce8aa694
al-fred-o <agomez@g.hmc.edu>
2022-10-13 16:34:56 -0700

    Updating evaluation to refactor event labeling

--- compare_histograms.m
+++ compare_histograms.m
@@ -8,54 +8,39 @@ real_times_10xsnr = real_event_data(:,1);
 real_event_data = table2array(readtable("test_100xsnr_var_events.csv"));
 real_times_100xsnr = real_event_data(:,1);
 
-%% Jump Detection Evaluation
-% Keep track of double jumps
-single_jump_100xsnr_var = false(size(jumps_measured_100xsnr_var(:,1)));
-multi_jump_100xsnr_var = false(size(single_jump_100xsnr_var));
-fp_jump_100xsnr_var = false(size(single_jump_100xsnr_var));
+%% Detected Jump Labeling
+% For each measured/detected event, label as 
+% no-jump(0), single-jump(1) or multi-jump
+labels = LabelDetectedJumps(jumps_measured_100xsnr_var, real_times_100xsnr, tmeas, tjump);
 
-% Iterate through detected jumps and assign them as either a false positive
-% or true negative
-for measured_event_i = 1:size(jumps_measured_100xsnr_var,1)
-
-    % Build a range around each detected jump
-    detected_event = jumps_measured_100xsnr_var(measured_event_i,1);
-    lower_range = jumps_measured_100xsnr_var(measured_event_i,2);
-    upper_range = jumps_measured_100xsnr_var(measured_event_i,3);
-    
-    % Iterate through real events and see if any fall within jump range
-    for real_event_i=1:length(real_times_100xsnr)
-        real_event = real_times_100xsnr(real_event_i);
-        if lower_range <= real_event && real_event <= upper_range
-            single_jump_100xsnr_var(measured_event_i) = true;
-        end
-    end
-
-    real_count = 0;
-    for real_event_i=1:length(real_times_100xsnr)
-        real_event = real_times_100xsnr(real_event_i);
-        if lower_range-tmeas-tjump <= real_event && real_event <= upper_range+tmeas+tjump
-            real_count = real_count + 1;
-            if real_count > 1
-                multi_jump_100xsnr_var(measured_event_i) = true;
-                single_jump_100xsnr_var(measured_event_i) = false;
-            end
-        end
-    end
-end
-
+% choose two moments to plot labeling against
 ind1=3;
 ind2=8;
 figure; hold on
 % plot(jump_stats_1xsnr_var(:,ind1),jump_stats_1xsnr_var(:,ind2),'.','MarkerSize',7);
 % plot(jump_stats_10xsnr_var(:,ind1),jump_stats_10xsnr_var(:,ind2),'.','MarkerSize',7);
-plot(jump_stats_100xsnr_var(single_jump_100xsnr_var,ind1),jump_stats_100xsnr_var(single_jump_100xsnr_var,ind2),'.','MarkerSize',7);
-plot(jump_stats_100xsnr_var(multi_jump_100xsnr_var,ind1),jump_stats_100xsnr_var(multi_jump_100xsnr_var,ind2),'.','MarkerSize',7);
+plot(jump_stats_100xsnr_var(labels == 1,ind1),jump_stats_100xsnr_var(labels == 1,ind2),'.','MarkerSize',7);
+plot(jump_stats_100xsnr_var(labels == 2,ind1),jump_stats_100xsnr_var(labels==2,ind2),'.','MarkerSize',7);
+plot(jump_stats_100xsnr_var(labels == 0,ind1),jump_stats_100xsnr_var(labels==0,ind2),'.','MarkerSize',7);
 plot(jump_stats_snrboost_fixed(:,ind1),jump_stats_snrboost_fixed(:,ind2),'.','MarkerSize',7);
-legend('Variable event rate - single jumps','Variable event rate - multi jumps','Fixed event rate - single jumps');
+legend('Variable event rate - single jumps','Variable event rate - multi jumps', 'Variable event rate - False Jumps','Fixed event rate - single jumps');
+%% Histogram Plotting
 
 figure;
-h1=histogram(jump_stats_100xsnr_var(multi_jump_100xsnr_var,ind1),100); hold on
-h2=histogram(jump_stats_snrboost_fixed(:,ind1),100,'BinEdges',h1.BinEdges);
-legend('Variable event rate - multi jumps','Fixed event rate - single jumps');
-pdist2(h1.BinCounts,h2.BinCounts)
+for moment_i = 1:4
+    subplot(2,2, moment_i)
+    h1=histogram(jump_stats_100xsnr_var(labels==2,moment_i),100); hold on
+    h2=histogram(jump_stats_100xsnr_var(labels==1,moment_i),100,'BinEdges',h1.BinEdges);
+    legend('Variable event rate - multi jumps','Variable event rate - single jumps');
+    pdist2(h1.BinCounts,h2.BinCounts)
+end
+% Plot of FWHM comparing multi-events to fixed rate-events
+% figure;
+% h1=histogram(jump_stats_100xsnr_var(labels==2,8),100); hold on
+% h2=histogram(jump_stats_snrboost_fixed(:,8),100,'BinEdges',h1.BinEdges);
+% legend('Variable event rate - multi jumps','Fixed event rate - single jumps');
+% pdist2(h1.BinCounts,h2.BinCounts)
+
+
+
+
--- evaluate_detections.m
+++ evaluate_detections.m
@@ -2,113 +2,69 @@
 % Assumes main function has been ran to collected detected jumps
 real_event_data = table2array(readtable("test_10xsnr_var_events.csv"));
 real_times = real_event_data(:,1);
-% detected_times = jumps_measured(:,1);
+
 jumps_measured = table2array(readtable("10xSNR_measured.csv"));
 jumps_detected = table2array(readtable("10xSNR_detected.csv"));
-
 jump_stats = table2array(readtable("10xSNR_stats.csv"));
 
 %% Jump Detection Evaluation
-detected_times = jumps_detected(:,1);
-detected_starts = jumps_detected(:,2);
-detected_ends = jumps_detected(:,3);
-% Keep track of true positives and false positives
-TP = 0;
-FP = 0;
-labeling = zeros(size(detected_times));
+% Only evaluates our algorithms ability to detect any
+% regardless if they are single or multi-events
 
-% Iterate through detected jumps and assign them as either a false positive
-% or true negative
-for detected_event_i = 1:length(detected_times)
+labeling = LabelDetectedJumps(jumps_detected, real_times, tmeas, tjump);
+TP1 = length(jumps_detected(labeling >= 1));
+FP1 = length(jumps_detected(labeling == 0));
+FN1 = length(real_times) - TP1;
+precision1 = TP1/(TP1+FP1);
+recall1 = TP1/(TP1+FN1);
 
-    % Build a range around each detected jump
-    detected_event = detected_times(detected_event_i);
-    lower_range = detected_starts(detected_event_i);
-    upper_range = detected_ends(detected_event_i);
-    is_real = false;
-    
-    % Iterate through real events and see if any fall within jump range
-    real_count = 0;
-    for real_event_i=1:length(real_times)
-        real_event = real_times(real_event_i);
-        if lower_range <= real_event && real_event <= upper_range
-            is_real = true;
-            real_count = real_count + 1;
-        end
-    end
-
-    % If matched with a real event, record positions and counts
-    if is_real
-        TP = TP + 1;
-    else
-        FP = FP + 1;
-    end
-    labeling(detected_event_i) = real_count;
-
-end
-
-FN = length(real_times) - sum(labeling);
-[TP FP FN]
+% beta = 2 will bias toward recall
+% beta = 0.5 will bias precission
+beta = 0.5;
+Fscore1 =(1+beta)^2*(precision1*recall1)/(beta^2*precision1+recall1);
 
 
 %% Jump Filtering Evaluation
-detected_times = jumps_measured(:,1);
-detected_starts = jumps_measured(:,2);
-detected_ends = jumps_measured(:,3);
+% Evaluates our algorithms ability to detect any event after pre-filtering
+% regardless if they are single or multi-events 
+labeling = LabelDetectedJumps(jumps_measured, real_times, tmeas, tjump);
+TP2 = length(jumps_measured(labeling >= 1));
+FP2 = length(jumps_measured(labeling == 0));
+FN2 = length(real_times) - TP2;
+precision2 = TP2/(TP2+FP2);
+recall2 = TP2/(TP2+FN2);
 
-% Keep track of true positives and false positives
-TP = 0;
-FP = 0;
-labeling = zeros(size(detected_times));
-
-% Iterate through detected jumps and assign them as either a false positive
-% or true negative
-for detected_event_i = 1:length(detected_times)
-
-    % Build a range around each detected jump
-    detected_event = detected_times(detected_event_i);
-    lower_range = detected_starts(detected_event_i);
-    upper_range = detected_ends(detected_event_i);
-    is_real = false;
-    
-    % Iterate through real events and see if any fall within jump range
-    real_count = 0;
-    for real_event_i=1:length(real_times)
-        real_event = real_times(real_event_i);
-        if lower_range <= real_event && real_event <= upper_range
-            is_real = true;
-            real_count = real_count + 1;
-        end
-    end
-
-    % If matched with a real event, record positions and counts
-    if is_real
-        TP = TP + 1;
-    else
-        FP = FP + 1;
-    end
-    labeling(detected_event_i) = real_count;
-
-end
-
-FN = length(real_times) - sum(labeling);
-[TP FP FN]
+% beta = 2 will bias toward recall
+% beta = 0.5 will bias precission
+beta = 0.5;
+Fscore2 =(1+beta)^2*(precision2*recall2)/(beta^2*precision2+recall2);
 
 
 
-%% 
+%% Clustering/PostFiltering Process
+% Note that Jump Stats and Jump Measures need to be defined at this point
+% jumps_measured = jumps_measured_100xsnr_var;
+% jump_stats = jump_stats_100xsnr_var;
 clustering;
-PostFilter = PostFiltering(jump_stats, 75);
+% Running clusterin gshould define the final_clusters variable
+% used in next section
+PostFilter = PostFiltering(jump_stats, 0);
 
-%%
-single_event_count = length(detected_times(labeling == 1 & PostFilter == 1));
-multi_event_count = length(detected_times(labeling > 1 & PostFilter == 1));
-[single_event_count/(single_event_count+multi_event_count)]
+%% Calculate Evaluation
+% Note that at this point, real_times needs to be defined
 
-cluster = 2;
-for i = 1:cluster
-    single_event_count_c = length(detected_times(final_clusters >= i & labeling == 1 & PostFilter == 1));
-    multi_event_count_c = length(detected_times(final_clusters >= i & labeling > 1 & PostFilter == 1));
-%     [single_event_count multi_event_count single_event_count_c multi_event_count_c]
-    [single_event_count_c/(single_event_count_c+multi_event_count_c)]
-end
+% TP: detected single-jump event that are in the final cluster
+% FP: Events in our final cluster that are either multi-event(label == 2)
+% or no-event(label == 0)
+TP3 = length(jumps_measured(final_clusters >= 1 & labels == 1 & PostFilter == 1));
+FP3 = length(jumps_measured(final_clusters >= 1 & labels ~= 1 & PostFilter == 1));
+
+% FN: real-events that we did not detect as single-jumps
+FN3 = length(real_times) - TP3;
+precision3 = TP/(TP+FP);
+recall3 = TP3/(TP3+FN3);
+
+% beta = 2 will bias toward recall
+% beta = 0.5 will bias precission
+beta = 0.5;
+Fscore =(1+beta)^2*(precision3*recall3)/(beta^2*precision3+recall3);
