f47f1235351820e30fd58201a14edcb15a8a16d2
al-fred-o <agomez@g.hmc.edu>
2023-05-25 13:43:52 -0700

    Add helper plotting funciton

---
+++ jump_detection/plotting.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+from .utils import calculate_median_jump
+
+def plot_segment(segment):
+    """
+    Plots a histogram.
+    
+    Parameters:
+    
+    """
+    fig, (ax1, ax3) = plt.subplots(2,1)
+
+    column1 = segment.original[:, 0]
+    column2 = segment.original[:, 1]
+
+    # Plot the first column on the left y-axis
+    ax1.plot(column1, 'b-', label='Column 1')
+    ax1.set_xlabel('Index')
+    ax1.set_ylabel('Mode 1', color='b')
+    ax1.tick_params('y', colors='b')
+
+    # Create a twin axes object and plot the second column on the right y-axis
+    ax2 = ax1.twinx()
+    ax2.plot(column2, 'r-', label='Column 2')
+    ax2.set_ylabel('Mode 2', color='r')
+    ax2.tick_params('y', colors='r')
+
+
+    ax3.plot(segment.Fstats)
+    plt.show()
+
+def plot_jump_signature(segment_list, labels):
+    median_jump = calculate_median_jump(segment_list, labels)
+
+    fig, ax1 = plt.subplots(1,1)
+
+    column1 = median_jump[:, 0]
+    column2 = median_jump[:, 1]
+
+    ax1.set_title("Jump Signature")
+    # Plot the first column on the left y-axis
+    ax1.plot(column1, 'b-', label='Column 1')
+    ax1.set_xlabel('Index')
+    ax1.set_ylabel('Mode 1', color='b')
+    ax1.tick_params('y', colors='b')
+
+    # Create a twin axes object and plot the second column on the right y-axis
+    ax2 = ax1.twinx()
+    ax2.plot(column2, 'r-', label='Column 2')
+    ax2.set_ylabel('Mode 2', color='r')
+    ax2.tick_params('y', colors='r')
+
+    plt.show()
