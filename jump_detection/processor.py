5ca30b2ce02577bb2aeba3565a2ead0a54d8331b
al-fred-o <agomez@g.hmc.edu>
2023-06-12 23:16:06 -0700

    Processor whitespace fix

--- jump_detection/processor.py
+++ jump_detection/processor.py
@@ -4,6 +4,7 @@ from .segment import Segment
 from .utils import *
 from .rolling_Ftest import *
 
+
 class TimeSeriesProcessor:
     """
     Class for processing time series data.
@@ -14,7 +15,7 @@ class TimeSeriesProcessor:
         The window size for moving calculations.
     """
 
-    def __init__(self, window_size = 10, gap_size = 0) -> None:
+    def __init__(self, window_size=10, gap_size=0) -> None:
         """
         Initialize a TimeSeriesProcessor.
 
@@ -37,23 +38,26 @@ class TimeSeriesProcessor:
         """
         self.data = pd.read_csv(filename).values
 
-
     def process_data(self):
         """
         Process the time series data, creating segments around the peaks.
         """
-        moving_fstat = rolling_F_statistic(self.data, self.window_size, self.window_size, self.gap_size)
+        moving_fstat = rolling_F_statistic(
+            self.data, self.window_size, self.window_size, self.gap_size)
         peaks = find_peaks_in_data(moving_fstat)
         self.moving_fstats = moving_fstat
 
         # Segment out
         # Offset time segments duw to different in original timeseries and fstat
         t_offset = int(self.window_size+self.gap_size/2)
-        original_segments = segment_data(self.window_size, self.gap_size, peaks + t_offset, self.data)
-        fstat_segments = segment_data(self.window_size,self.gap_size, peaks,  moving_fstat)
-        
-        self.segments = [Segment(original, Fstats) for original, Fstats in zip(original_segments, fstat_segments)]
-    
+        original_segments = segment_data(
+            self.window_size, self.gap_size, peaks + t_offset, self.data)
+        fstat_segments = segment_data(
+            self.window_size, self.gap_size, peaks,  moving_fstat)
+
+        self.segments = [Segment(original, Fstats) for original, Fstats in zip(
+            original_segments, fstat_segments)]
+
     def get_all_features(self):
         """
         Iterate through all collected event segments and return a ndarray with all the
@@ -67,12 +71,14 @@ class TimeSeriesProcessor:
         for i, segment in enumerate(self.segments):
             jump_features.append(segment.features)
         jump_features = np.array(jump_features)
-        return normalize_features(jump_features)
-    
+        normalized_features, self.valid_jumps = normalize_features(
+            jump_features)
+        return normalized_features
+
     def get_all_diffs(self):
         diffs = []
         for segment in self.segments:
             segment.calculate_freq_shift(self.window_size)
             diffs.append(segment.diff)
         diffs = np.array(diffs)
-        return diffs
+        return diffs[self.valid_jumps]
